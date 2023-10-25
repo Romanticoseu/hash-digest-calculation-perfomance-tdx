6ebfc296be9d7ab24c7bcac0d3b2e84290894123
Romanticoseu <1251448395@qq.com>
2023-10-25 21:49:39 +0800

    test

--- client/client.py
+++ client/client.py
@@ -1,19 +1,48 @@
 import socket
 import os
+import time
 
 # Define the server address (host and port)
-server_host = os.getenv("SERVER_HOST_NAME")  # Change this to the IP of the server
+# server_host = os.getenv("SERVER_HOST_NAME") 
+server_host = '127.0.0.1'  # Change this to the server's IP
 server_port = 12345
 
 # Create a UDP socket
 client_socket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
 
-while True:
-    message = input("Enter a message to send to the server: ")
+# Specify the path to the file containing data
+data_file_path = "test.txt"
 
-    # Send the message to the server
-    client_socket.sendto(message.encode('utf-8'), (server_host, server_port))
+# Define the chunk size for reading and sending data
+chunk_size = 1024  # You can adjust this value as needed
 
-    # Receive the response from the server
-    response, server_address = client_socket.recvfrom(1024)
-    print(f"Received from {server_address}: {response.decode('utf-8')}")
+
+# Open the data file for reading
+with open(data_file_path, 'rb') as data_file:
+    # Create a UDP socket
+    client_socket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+
+    # Record transfer time start
+    transfer_start_time = time.time()
+
+    while True:
+        data_chunk = data_file.read(chunk_size)
+        if not data_chunk:
+            break  # End of file reached, exit the loop
+
+        # Send the binary data chunk to the server
+        client_socket.sendto(data_chunk, (server_host, server_port))
+
+    # Receive the final hash digest from the server
+    response, server_address = client_socket.recvfrom(64)  # Adjust buffer size as needed
+
+    # Record transfer time end
+    transfer_end_time = time.time()
+
+    # Print the received hash digest
+    print(f"Received hash digest: {response.decode('utf-8')}")
+
+    # Calculate and print times
+    print(f"Transfer time: {transfer_end_time - transfer_start_time}")
+
+
--- server/server.py
+++ server/server.py
@@ -1,5 +1,6 @@
 import socket
 import hashlib
+import time
 
 # Define the server address (host and port)
 server_host = '0.0.0.0'  # Listen on all available network interfaces
@@ -9,17 +10,30 @@ server_port = 12345
 server_socket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
 server_socket.bind((server_host, server_port))
 
+# Increase the buffer size for receiving data
+buffer_size = 1024
+
 print(f"Server listening on {server_host}:{server_port}")
 
 while True:
     # Receive data from the client
-    data, client_address = server_socket.recvfrom(1024)
+    data, client_address = server_socket.recvfrom(buffer_size)
+
+    # Record server receive time
+    server_receive_time = time.time()
+
     message = data.decode('utf-8')
     print(f"Received from {client_address}: {message}")
 
     # Calculate the SHA-256 hash of the message
+    hash_start_time = time.perf_counter()  # Record hash calculation start time
     sha256_hash = hashlib.sha256(message.encode()).hexdigest()
+    hash_end_time = time.perf_counter()  # Record hash calculation end time
     print(f"SHA-256 Hash: {sha256_hash}")
 
     # Send the hash back to the client
     server_socket.sendto(sha256_hash.encode('utf-8'), client_address)
+
+    # Calculate and print times
+    print(f"Server receive time: {server_receive_time}")
+    print(f"Hash calculation time: {hash_end_time - hash_start_time}")
